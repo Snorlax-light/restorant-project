eb83b042c4ee6937c77fa620b93688da5a77bd6d
Snorlax-light <johnsulaiman4@gmail.com>
2025-06-12 08:15:11 -0700

    progress day 2

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,10 +4,10 @@
         <h1>Sign Up</h1>
 
         <div class="register">
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <input type="text" v-model="name" placeholder="Name" />
+            <input type="email" v-model="email" placeholder="Email" />
+            <input type="password" v-model="password" placeholder="Password" />
+            <button v-on:click="signUp">Sign Up</button>
         </div>
        
 
@@ -18,9 +18,36 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data()
+    {
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp(){
+            let result = await axios.post("http://localhost:3000/user",{
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+
+            console.warn(result);
+            if(result.status === 201){
+                alert("User registered successfully")
+                localStorage.setItem("user-info", JSON.stringify(result.data))
+            }   
+        }
+    }
+
 }
+
+
 </script>
 
 <style>
